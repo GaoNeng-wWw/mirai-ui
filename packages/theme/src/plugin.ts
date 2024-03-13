f1a356f666e9d5b274af1d455683533d2ffa4af6
GaoNeng-wWw <gaonenggg@outlook.com>
2024-03-13 20:49:34 +0800

    fix(theme): build error

--- packages/theme/src/plugin.ts
+++ packages/theme/src/plugin.ts
@@ -12,16 +12,16 @@ interface Config {
 interface Resoved{
   variants: {name: string; definition: string[]}[]
   utilities: Record<string, Record<string, any>>
-  colors: Record<string, ({opacityValue, opacityVariable}: {opacityValue: string; opacityVariable: string}) => string>;
+  colors: Record<string, ({ opacityValue, opacityVariable }: {opacityValue: string; opacityVariable: string}) => string>;
 }
 
 function removeDefaultKeys<T extends Object>(obj: T) {
   const newObj = {};
 
   for (const key in obj) {
-    if (key.endsWith("-DEFAULT")) {
+    if (key.endsWith('-DEFAULT')) {
       // @ts-ignore
-      newObj[key.replace("-DEFAULT", "")] = obj[key];
+      newObj[key.replace('-DEFAULT', '')] = obj[key];
       continue;
     }
     // @ts-ignore
@@ -34,9 +34,9 @@ const flattenThemeObject = <TTarget>(obj: TTarget) =>
   removeDefaultKeys(
     flatten(obj, {
       safe: true,
-      delimiter: "-",
+      delimiter: '-',
     }) as Object,
-);
+  );
 
 const resolve = (
   themes: Record<string, ConfigTheme> = {},
@@ -47,19 +47,19 @@ const resolve = (
     utilities: {},
     colors: {},
     variants: []
-  }
-  const parsedColorsCache:Record<string, any> = {}
-  for (const [themeName, {colors, extend}] of Object.entries(themes)){
+  };
+  const parsedColorsCache:Record<string, any> = {};
+  for (const [themeName, { colors, extend }] of Object.entries(themes)) {
     let cssSelector = `.${themeName},[data-theme="${themeName}"]`;
-    const scheme = themeName === "light" || themeName === "dark" ? themeName : extend;
+    const scheme = themeName === 'light' || themeName === 'dark' ? themeName : extend;
     if (themeName === defaultTheme) {
       cssSelector = `:root,${cssSelector}`;
     }
 
     resolved.utilities[cssSelector] = scheme
       ? {
-          "color-scheme": scheme,
-        }
+        'color-scheme': scheme,
+      }
       : {};
     const flatColors = flattenThemeObject(colors) as Record<string, string>;
     resolved.variants.push({
@@ -67,7 +67,7 @@ const resolve = (
       definition: [`&.${themeName}`, `&[data-theme='${themeName}']`],
     });
     for (const [colorName, colorValue] of Object.entries(flatColors)) {
-      if (!colorValue) return;
+      if (!colorValue) { return; }
 
       try {
         const parsedColor =
@@ -82,11 +82,11 @@ const resolve = (
         
         resolved.utilities[cssSelector]![colorVar] = `${h} ${s}% ${l}%`;
         
-        if (typeof defaultAlphaValue === "number") {
+        if (typeof defaultAlphaValue === 'number') {
           resolved.utilities[cssSelector]![opacityVar] = defaultAlphaValue.toFixed(2);
         }
         
-        resolved.colors[colorName] = ({opacityVariable, opacityValue}) => {
+        resolved.colors[colorName] = ({ opacityVariable, opacityValue }) => {
 
           if (!isNaN(+opacityValue)) {
             return `hsl(var(${colorVar}) / ${opacityValue})`;
@@ -100,55 +100,55 @@ const resolve = (
         };
       } catch (error: any) {
         // eslint-disable-next-line no-console
-        console.log("error", error?.message);
+        console.log('error', error?.message);
       }
     }
   }
   return resolved;
-}
+};
 
 const omit = (obj:Object, key: string[]) => {
   const newObj:Record<string, any> = {};
-  for (const [k,v] of Object.entries(obj)){
-    if (key.includes(k)){
+  for (const [k, v] of Object.entries(obj)) {
+    if (key.includes(k)) {
       continue;
     }
     newObj[k] = v;
   }
   return newObj;
-}
+};
 
 export const miraiUiPlugin = (
   config: Config={}
 ) => {
-  const {prefix, themes} = config;
+  const { prefix, themes } = config;
   const userLight = themes?.light ?? {};
   const userDark = themes?.dark ?? {};
-  const otherTheme = omit(config.themes ?? {}, ['light', 'dark'])
+  const otherTheme = omit(config.themes ?? {}, ['light', 'dark']);
   const light:ConfigTheme = {
     colors: {
       ...userLight.colors,
       ...semanticColor.light
     }
-  }
+  };
   const dark:ConfigTheme = {
     colors: {
       ...userDark.colors,
       ...semanticColor.dark
     }
-  }
+  };
   const theme = {
     light,
     dark,
     ...otherTheme
-  }
-  const resolved = resolve(theme, 'light', prefix)
-  return plugin((api)=>{
-    api.addUtilities({...resolved?.utilities});
+  };
+  const resolved = resolve(theme, 'light', prefix);
+  return plugin((api) => {
+    api.addUtilities({ ...resolved?.utilities });
     resolved?.variants.forEach((variant) => {
       api.addVariant(variant.name, variant.definition);
-    })
-  },{
+    });
+  }, {
     theme:{
       extend:{
         // @ts-ignore
@@ -158,5 +158,5 @@ export const miraiUiPlugin = (
         },
       }
     }
-  })
-}
+  });
+};
