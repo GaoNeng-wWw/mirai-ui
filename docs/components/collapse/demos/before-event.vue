fce484395f1b58ccb9f1e995006e48ea169b800b
unknown <gaonenggg@outlook.com>
2024-04-15 20:15:18 +0800

    feat(collapse): beforeOpen & beforeClose

---
+++ docs/components/collapse/demos/before-event.vue
@@ -0,0 +1,30 @@
+<template>
+  <collapse v-model="actived" @before-open="beforeOpen" @before-close="beforeClose">
+    <collapse-item title="Header-1" key="item-1">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum incidunt dicta voluptas praesentium sapiente tempora numquam quidem, possimus autem quisquam tenetur voluptatibus, similique saepe officia amet velit ratione! Explicabo, voluptatum?
+    </collapse-item>
+    <collapse-item title="prevent-open" key="prevent-open">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum incidunt dicta voluptas praesentium sapiente tempora numquam quidem, possimus autem quisquam tenetur voluptatibus, similique saepe officia amet velit ratione! Explicabo, voluptatum?
+    </collapse-item>
+    <collapse-item title="prevent-close" key="prevent-close">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum incidunt dicta voluptas praesentium sapiente tempora numquam quidem, possimus autem quisquam tenetur voluptatibus, similique saepe officia amet velit ratione! Explicabo, voluptatum?
+    </collapse-item>
+  </collapse>
+</template>
+
+<script lang="ts" setup>
+import {Collapse, CollapseItem} from '@mirai-ui/vue-components';
+import { ref } from 'vue';
+const actived = ref(['item-1']);
+const count = ref(0)
+const beforeOpen = (key, f) => {
+  if (key === 'prevent-open') {
+    f()
+  }
+}
+const beforeClose = (key, f) => {
+  if (key === 'prevent-close') {
+    f()
+  }
+}
+</script>
--- packages/vue-components/collapse/src/collapse.type.ts
+++ packages/vue-components/collapse/src/collapse.type.ts
@@ -6,4 +6,6 @@ export type CollapseProvide = {
   onItemClick: (key: string | number | symbol)=>void;
   open: (key: string | number | symbol) => void,
   close: (key: string | number | symbol) => void,
+  beforeOpen: (key: string | number | symbol, next: ()=>void) => boolean,
+  beforeClose: (key: string | number | symbol, next: ()=>void) => boolean,
 }
