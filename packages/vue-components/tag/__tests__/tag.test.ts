ae64740cab8b10717487a3c00d545ece07fdb31d
GaoNeng-wWw <gaonenggg@outlook.com>
2024-04-19 13:35:53 +0800

    test(vue-component): tag unit test

--- packages/vue-components/tag/__tests__/tag.test.ts
+++ packages/vue-components/tag/__tests__/tag.test.ts
@@ -1,10 +1,29 @@
-
-import { describe, it, expect} from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { mount } from '@vue/test-utils';
 import tag from '../src/tag.vue';
-describe('camelCase(name)', ()=>{
-    it('should to be defined', ()=>{
-        expect(mount(tag)).toBeDefined()
-    })
-})
-    
+describe('tag', () => {
+  it('should to be defined', () => {
+    expect(mount(tag)).toBeDefined();
+  });
+  it('event', async () => {
+    const f = vi.fn();
+    const wrapper = mount(tag, {
+      props: {
+        onClick: f
+      }
+    });
+    await wrapper.trigger('click');
+    expect(f).toBeCalled();
+    expect(f).toHaveBeenCalledOnce();
+  });
+  it('props', async () => {
+    const wrapper = mount(tag, {
+      props: {
+        colors: 'primary'
+      }
+    });
+    expect(wrapper.classes()).contain('bg-primary-100');
+    await wrapper.setProps({ colors: 'danger' });
+    expect(wrapper.classes()).contain('bg-danger-100');
+  });
+});
