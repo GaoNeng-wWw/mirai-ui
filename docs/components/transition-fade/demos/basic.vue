da8e1b1448833f2ac2d9c01f672e0431b371061c
GaoNeng-wWw <gaonenggg@outlook.com>
2024-06-02 15:20:47 +0800

    feat(transition): fade transition

---
+++ docs/components/transition-fade/demos/basic.vue
@@ -0,0 +1,16 @@
+<template>
+  <transition-fade>
+    <div v-if="visible" class="w-32 h-32 bg-primary text-primary-foreground">
+      hello-world
+    </div>
+  </transition-fade>
+  <m-button @click="visible = !visible">
+    切换
+  </m-button>
+</template>
+
+<script lang="ts" setup>
+import {TransitionFade, Button as MButton} from '@miraiui-org/vue-components';
+import { ref } from 'vue';
+const visible = ref(false);
+</script>
---
+++ packages/vue-components/transition-fade/src/transitionFade.vue
@@ -0,0 +1,28 @@
+<template>
+  <transition name="fade" class="transition"
+    @before-enter="beforeEnter"
+    @before-leave="beforeLeave"
+    @enter="onEnter"
+    @leave="onLeave"
+  >
+    <slot />
+  </transition>
+</template>
+<script setup lang="ts">
+import { RendererElement } from 'vue';
+
+const COMPONENT_NAME='MTransitionFade';
+defineOptions({
+  name: COMPONENT_NAME
+});
+const beforeEnter = (el: RendererElement) => el.style.opacity = '0';
+const onEnter = (el: RendererElement) => {
+  requestAnimationFrame(() => {
+    el.style.opacity = '1';
+  });
+};
+const beforeLeave = (el: RendererElement) => {
+  el.style.opacity = '1';
+};
+const onLeave = (el: RendererElement) => el.style.opacity = '0';
+</script>
