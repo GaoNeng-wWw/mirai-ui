cb6f5123eef0489f2a79beccb92a85bde61c3df1
GaoNeng-wWw <gaonenggg@outlook.com>
2024-06-05 20:07:02 +0800

    feat(helper): only child

---
+++ packages/vue-components/helper/src/ChildOnly.ts
@@ -0,0 +1,63 @@
+import { FORWARD_REF_INJECTION_KEY, useForwardRefDirective } from '@miraiui-org/hooks';
+import { defineComponent, inject, VNode, Fragment, createVNode, cloneVNode, withDirectives, warn } from 'vue';
+import { NOOP, isObject } from '@vue/shared';
+
+const name = 'MChildOnly';
+export const ChildOnly = defineComponent({
+  name,
+  setup(_, { slots, attrs }) {
+    const forwardInject = inject(FORWARD_REF_INJECTION_KEY);
+    const forwardRefDirective = useForwardRefDirective(
+      forwardInject?.setForwardRef ?? NOOP
+    );
+    return () => {
+      const defaultSlot = slots.default?.(attrs);
+      if (!defaultSlot) { return null; }
+
+      if (defaultSlot.length > 1) {
+        warn(`${name} requires exact only one valid child.`);
+        return null;
+      }
+
+      const firstLegitNode = findFirstLegitChild(defaultSlot);
+      if (!firstLegitNode) {
+        warn(`${name} no valid child node found`);
+        return null;
+      }
+
+      return withDirectives(cloneVNode(firstLegitNode!, attrs), [
+        [forwardRefDirective],
+      ]);
+    };
+  }
+});
+
+function findFirstLegitChild(node: VNode[] | undefined): VNode | null {
+  if (!node) { return null; }
+  const children = node as VNode[];
+  for (const child of children) {
+
+    /**
+     * when user uses h(Fragment, [text]) to render plain string,
+     * this switch case just cannot handle, when the value is primitives
+     * we should just return the wrapped string
+     */
+    if (isObject(child)) {
+      switch (child.type) {
+      case Comment:
+        continue;
+      case Text:
+      case 'svg':
+        return wrapTextContent(child);
+      case Fragment:
+        return findFirstLegitChild(child.children as VNode[]);
+      default:
+        return child;
+      }
+    }
+    return wrapTextContent(child);
+  }
+  return null;
+}
+
+const wrapTextContent = (child: string | VNode) => createVNode('span', null, child);
