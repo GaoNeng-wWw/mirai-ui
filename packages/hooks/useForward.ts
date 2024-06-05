c792d3af8040d429335f231599e71fdb4cc1c389
GaoNeng-wWw <gaonenggg@outlook.com>
2024-06-05 20:05:47 +0800

    feat(hooks): add hook internal use

---
+++ packages/hooks/useForward.ts
@@ -0,0 +1,39 @@
+import { provide } from 'vue'
+
+import type { InjectionKey, ObjectDirective, Ref } from 'vue'
+
+type ForwardRefSetter = <T>(el: T) => void
+
+export type ForwardRefInjectionContext = {
+  setForwardRef: ForwardRefSetter
+}
+
+export const FORWARD_REF_INJECTION_KEY: InjectionKey<ForwardRefInjectionContext> =
+  Symbol('elForwardRef')
+
+export const useForwardRef = <T>(forwardRef: Ref<T | null>) => {
+  const setForwardRef = (el: T) => {
+    forwardRef.value = el
+    console.log(el, forwardRef.value);
+  }
+
+  provide(FORWARD_REF_INJECTION_KEY, {
+    setForwardRef,
+  } as ForwardRefInjectionContext)
+}
+
+export const useForwardRefDirective = (
+  setForwardRef: ForwardRefSetter
+): ObjectDirective => {
+  return {
+    mounted(el) {
+      setForwardRef(el)
+    },
+    updated(el) {
+      setForwardRef(el)
+    },
+    unmounted() {
+      setForwardRef(null)
+    },
+  }
+}
