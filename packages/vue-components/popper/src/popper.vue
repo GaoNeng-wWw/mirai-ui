28e402caa5ec2a57ec7eec00e83d3fd76ce9fbdc
GaoNeng-wWw <gaonenggg@outlook.com>
2024-06-06 09:31:51 +0800

    feat: delete modelValue

--- packages/vue-components/popper/src/popper.vue
+++ packages/vue-components/popper/src/popper.vue
@@ -9,12 +9,10 @@ import { provide, ref } from 'vue';
 import { KEY, PopperContext, PopperProp } from './popper.props';
 
 const props = defineProps(PopperProp);
-const [modelValue] = defineModel<boolean>();
 const context: PopperContext = {
   trigger: ref(),
   content: ref(),
   safePoly: ref(),
-  visible: modelValue,
   virtualTrigger: ref(props.virtualTrigger)
 };
 
