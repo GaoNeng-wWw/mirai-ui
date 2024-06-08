61d479c2df7b37020e8140c545c7b032a0d0597b
GaoNeng-wWw <gaonenggg@outlook.com>
2024-06-08 13:49:04 +0800

    fix: build error

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,7 +5,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-import Miraiui from '@miraiui-org/vue-components/index';
+import Miraiui from '@miraiui-org/vue-components';
 
 export default {
   extends: DefaultTheme,
