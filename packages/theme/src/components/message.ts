6f4f6479f63bff7d3aa473622faba6abba9da4a1
GaoNeng-wWw <gaonenggg@outlook.com>
2024-06-24 15:01:07 +0800

    feat(message): close icon

--- packages/theme/src/components/message.ts
+++ packages/theme/src/components/message.ts
@@ -1,7 +1,7 @@
 import { cva, VariantProps } from 'cva';
 
 export const message = cva({
-  base: 'fixed left-1/2 top-0 z-50 w-fit -translate-x-1/2 rounded-md border px-4 py-2 text-sm leading-none',
+  base: 'fixed left-1/2 top-0 z-50 flex w-fit -translate-x-1/2 items-center gap-1 rounded-md border px-4 py-2 text-sm leading-none',
   variants:{
     type: {
       info: 'bg-default-200 border-default-300 text-default-800',
@@ -11,6 +11,10 @@ export const message = cva({
       success: 'bg-success-100 border-success-200 text-success-800',
     }
   }
+});
+
+export const closeIcon = cva({
+  base: 'size-[1.125rem] cursor-pointer'
 });
 
 export type Mesasge = VariantProps<typeof message>;
